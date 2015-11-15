eca7eea928105fc61a9427653a10db378965d8d5
Michael Vollmer <vollmerm@indiana.edu>
2015-11-15 00:57:16 -0500

    Progress on MoreCoq.v

--- MoreCoq.v
+++ MoreCoq.v
@@ -77,8 +77,9 @@ Theorem silly_ex :
      evenb 3 = true ->
      oddb 4 = true.
 Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
+  intros.
+  apply H. apply H0. Qed.
+  (** [] *)
 
 (** To use the [apply] tactic, the (conclusion of the) fact
     being applied must match the goal _exactly_ -- for example, [apply]
@@ -111,12 +112,12 @@ Proof.
 (** Hint: you can use [apply] with previously defined lemmas, not
     just hypotheses in the context.  Remember that [SearchAbout] is
     your friend. *)
-
+(* SearchAbout rev. *)
 Theorem rev_exercise1 : forall (l l' : list nat),
      l = rev l' ->
      l' = rev l.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. symmetry. rewrite H. apply rev_involutive. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, optional (apply_rewrite)  *)
@@ -183,7 +184,7 @@ Example trans_eq_exercise : forall (n m o p : nat),
      (n + p) = m ->
      (n + p) = (minustwo o). 
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. apply trans_eq with (m). apply H0. apply H. Qed.
 (** [] *)
 
 
@@ -269,7 +270,7 @@ Example sillyex1 : forall (X : Type) (x y z : X) (l j : list X),
      y :: l = x :: j ->
      x = y.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. inversion H0. reflexivity. Qed.
 (** [] *)
 
 Theorem silly6 : forall (n : nat),
@@ -290,7 +291,7 @@ Example sillyex2 : forall (X : Type) (x y z : X) (l j : list X),
      y :: l = z :: j ->
      x = z.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. inversion H. Qed.
 (** [] *)
 
 (** While the injectivity of constructors allows us to reason
@@ -308,17 +309,18 @@ Proof. intros A B f x y eq. rewrite eq.  reflexivity.  Qed.
 (** **** Exercise: 2 stars, optional (practice)  *)
 (** A couple more nontrivial but not-too-complicated proofs to work
     together in class, or for you to work as exercises. *)
- 
+
+SearchAbout beq_nat.
 
 Theorem beq_nat_0_l : forall n,
    beq_nat 0 n = true -> n = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intro n. unfold beq_nat. induction n. intros. reflexivity. intros. inversion H. Qed.
 
 Theorem beq_nat_0_r : forall n,
    beq_nat n 0 = true -> n = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intro n. unfold beq_nat. induction n. intros. reflexivity. intros. inversion H. Qed.
 (** [] *)
 
 
@@ -377,13 +379,20 @@ Proof.
 (** **** Exercise: 3 stars (plus_n_n_injective)  *)
 (** Practice using "in" variants in this exercise. *)
 
+SearchAbout plus.
+
 Theorem plus_n_n_injective : forall n m,
      n + n = m + m ->
      n = m.
 Proof.
   intros n. induction n as [| n'].
+  Case "n = 0". intros. destruct m. reflexivity. inversion H.
+  Case "n = S m". intros. destruct m. inversion H. rewrite <- plus_n_Sm in H.
+  rewrite <- plus_n_Sm in H. simpl in H. inversion H. apply IHn' in H1. inversion H1. reflexivity.
+Qed.
+
     (* Hint: use the plus_n_Sm lemma *)
-    (* FILL IN HERE *) Admitted.
+
 (** [] *)
 
 (* ###################################################### *)
@@ -529,7 +538,11 @@ Proof.
 Theorem beq_nat_true : forall n m,
     beq_nat n m = true -> n = m.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intro n. induction n as [| n'].
+  Case "n = 0". simpl. intros m eq. destruct m. reflexivity. inversion eq.
+  Case "n = S m". simpl. intros m eq. destruct m. inversion eq. apply IHn' in eq.
+  apply f_equal. apply eq.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars, advanced (beq_nat_true_informal)  *)
@@ -702,7 +715,10 @@ Theorem index_after_last: forall (n : nat) (X : Type) (l : list X),
      length l = n ->
      index n l = None.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n X l eq. generalize dependent n.
+  induction l. intros. reflexivity.
+  destruct n. intros. inversion eq. intros. simpl. apply IHl. inversion eq. reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, advanced, optional (index_after_last_informal)  *)
@@ -725,7 +741,12 @@ Theorem length_snoc''' : forall (n : nat) (X : Type)
      length l = n ->
      length (snoc l v) = S n. 
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros. generalize dependent n. generalize dependent v.
+  induction l. intros. simpl. inversion H. reflexivity.
+  intros. destruct n. inversion H. simpl. apply f_equal. apply IHl. inversion H. reflexivity.
+Qed.
+  
+                                      
 (** [] *)
 
 (** **** Exercise: 3 stars, optional (app_length_cons)  *)
